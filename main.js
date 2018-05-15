21d7f8e26548039b807f8c4ed3e34a2c4bb1740a
Robyn Navarro <rnavarro182@gmail.com>
2018-05-15 15:55:43 -0700

    function for grabbing three images working

--- main.js
+++ main.js
@@ -3,16 +3,19 @@
 
 function composeTrio() {
     var imagesArrayCopy = imagesArray.slice(); //creates a copy of images array
-
+    var image1 = getRandomImage(imagesArrayCopy);
+    var image2 = getRandomImage(imagesArrayCopy);
+    var image3 = getRandomImage(imagesArrayCopy);
+    console.log(image1, image2, image3);
 }
 
 //gets a random image from imagesArray
-function getRandomImage() {
-    var index = getRandomIndex(imagesArray.length); //calls getRandomIndex function and assigns random number no larger that length of images array
-    var image = imagesArray[index]; //uses index to assign a random image from imagesArray
-    imagesArray.splice(index, 1); //removes image from array
-    //return image; //returns randomly selected image
+function getRandomImage(array) {
+    var index = getRandomIndex(array.length); //calls getRandomIndex function and assigns random number no larger that length of images array
+    var image = array[index]; //uses index to assign a random image from imagesArray
+    array.splice(index, 1); //removes image from array
     console.log('random image', image);
+    return image; //returns randomly selected image
 }
 
 // returns a random number no larger than the argument provided
@@ -20,4 +23,4 @@ function getRandomIndex(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-getRandomImage();
+composeTrio();
